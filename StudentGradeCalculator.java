9d22be75ec204f1ed89743d024d827c75170f9e8
MORTALE SWAPNIL <157574425+swapnilMortale@users.noreply.github.com>
2024-07-12 17:46:44 +0530

    Add files via upload

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student.grade.calculator;
+
+/**
+ *
+ * @author swapn
+ */
+public class StudentGradeCalculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Grade_Calculator();
+    }
+    
+}
